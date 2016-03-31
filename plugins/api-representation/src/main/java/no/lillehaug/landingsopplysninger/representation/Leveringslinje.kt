82598e19b148b3a621cec783fb2167413d13dd40
Marvin Bredal Lillehaug <marvin.lillehaug@kantega.no>
2016-03-31 22:43:58 +0200

    @JvmStatic

--- plugins/api-representation/src/main/java/no/lillehaug/landingsopplysninger/representation/Leveringslinje.kt
+++ plugins/api-representation/src/main/java/no/lillehaug/landingsopplysninger/representation/Leveringslinje.kt
@@ -4,7 +4,8 @@ import no.lillehaug.landingsopplysninger.api.Leveringslinje
 import java.time.LocalDate
 
 data class Leveringslinje (val fartøy: String, val landingsdato: LocalDate, val mottak: String, val fiskeslag: String, val tilstand: String, val størrelse: String, val kvalitet: String, val nettovekt: Double) {
-    companion object Convert {
+    companion object {
+        @JvmStatic
         fun fromApi(l: no.lillehaug.landingsopplysninger.api.Leveringslinje) : Leveringslinje {
             return no.lillehaug.landingsopplysninger.api.Leveringslinje(l.fartøy, l.landingsdato, l.mottak, l.fiskeslag, l.tilstand, l.størrelse, l.kvalitet, l.nettovekt)
         }
