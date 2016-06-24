ffe284f944a94a5335a23d874dca34015372453d
Marvin Bredal Lillehaug <marvin.lillehaug@kantega.no>
2016-06-24 21:18:40 +0200

    Runs, but no hot deploy

--- plugins/client/src/main/java/no/lillehaug/landingsopplysninger/ClientPlugin.java
+++ plugins/client/src/main/java/no/lillehaug/landingsopplysninger/ClientPlugin.java
@@ -10,16 +10,8 @@ import javax.servlet.Filter;
 public class ClientPlugin {
 
     @Export final Filter resourceServlet;
-    @Export final Filter vendorResource;
-    @Export final Filter vendorMapResource;
-    @Export final Filter appResource;
-    @Export final Filter appMappResource;
 
     public ClientPlugin(ServletBuilder servletBuilder) {
-        resourceServlet = servletBuilder.resourceServlet("/", getClass().getResource("/index.html"));
-        vendorResource = servletBuilder.resourceServlet("/dist/vendor.bundle.js", getClass().getResource("/assets/dist/vendor.bundle.js"));
-        vendorMapResource = servletBuilder.resourceServlet("/dist/vendor.bundle.js.map", getClass().getResource("/assets/dist/vendor.bundle.js.map"));
-        appResource = servletBuilder.resourceServlet("/dist/app.bundle.js", getClass().getResource("/assets/dist/app.bundle.js"));
-        appMappResource = servletBuilder.resourceServlet("/dist/app.bundle.js.map", getClass().getResource("/assets/dist/app.bundle.js.map"));
+        resourceServlet = servletBuilder.resourceServlet("/", getClass().getResource("/assets/index.html"));
     }
 }
--- plugins/client/src/main/web/hello.jsx
+++ plugins/client/src/main/web/hello.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 class Hello extends React.Component {
     render() {
-        return <h1>Hello</h1>
+        return <h1>halo</h1>
     }
 }
 ReactDOM.render(<Hello/>, document.getElementById('hello'));
