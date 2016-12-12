97e95a959e68423c67f0d223c324480b5e7d62c4
Marvin Bredal Lillehaug <marvin.lillehaug@kantega.no>
2016-12-12 20:10:50 +0100

    Moved frontend to module Using reststop:bootstrap

--- plugins/frontend/webpack.config.js
+++ plugins/frontend/webpack.config.js
@@ -4,21 +4,33 @@ var webpack = require('webpack');
 // TODO set NODE_ENV=production og inkluder UglifyJsPlugin
 module.exports = {
     entry: {
-        app : './src/main/webapp/main.js',
+        app : ['babel-polyfill', './src/main/js/main.ts'],
         vendor : ['react', 'react-dom', 'react-router', 'qwest', 'omnipotent', 'omniscient', 'immstruct', 'immutable']
     },
-    output: { path: __dirname + '/src/main/webapp/dist', filename: 'bundle.js' },
+    output: {
+        path: __dirname + '/target/classes/assets',
+        filename: 'bundle.js'
+    },
     devtool: 'source-map',
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: ["", ".ts", ".tsx", ".js", ".jsx"]
+    },
     module: {
         loaders: [
+            { test: /\.tsx?$/, loader: "babel!ts" },
             {
                 test: /.jsx?$/,
-                loader: 'babel-loader',
+                loader: 'babel',
                 exclude: /node_modules/,
                 query: {
                     presets: ['es2015', 'react']
                 }
             }
+        ],
+        preLoaders: [
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { test: /\.js$/, loader: "source-map-loader" }
         ]
     },
     plugins: [
