86b605719834644c636fa316aa0876b550adcb7b
Marvin Bredal Lillehaug <marvin.lillehaug@kantega.no>
2017-01-21 11:33:22 +0100

    webpack 2

--- plugins/frontend/webpack.config.js
+++ plugins/frontend/webpack.config.js
@@ -6,7 +6,15 @@ const DEBUG = process.env.NODE_ENV !== 'production';
 module.exports = {
     entry: {
         app : ['babel-polyfill', './src/main/js/main.tsx'],
-        vendor : ['react', 'react-dom', 'react-router', 'qwest', 'omnipotent', 'omniscient', 'immstruct', 'immutable']
+        vendor : [
+            'js-joda',
+            'react',
+            'react-dom',
+            'react-redux',
+            'redux',
+            'redux-thunk',
+            'web-request'
+        ]
     },
     output: {
         path: __dirname + '/target/classes/assets',
@@ -16,28 +24,30 @@ module.exports = {
     devtool: DEBUG ? 'cheap-module-eval-source-map' : 'source-map',
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: ["", ".ts", ".tsx", ".js", ".jsx"]
+        extensions: [".ts", ".tsx", ".js", ".jsx"]
     },
     module: {
-        loaders: [
-            { test: /\.tsx?$/, loader: "babel!ts" },
+        rules: [
             {
+                test: /\.tsx?$/,
+                use: ["babel-loader", "ts-loader"]
+            },{
                 test: /.jsx?$/,
-                loader: 'babel',
+                loader: 'babel-loader',
                 exclude: /node_modules/,
-                query: {
+                options: {
                     presets: ['es2015', 'react']
                 }
-            },
-            {test: /\.json$/, loader: 'json-loader'}
-        ],
-        preLoaders: [
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { test: /\.js$/, loader: "source-map-loader" }
+            }, {
+                test: /\.js$/,
+                enforce: "pre",
+                use: "source-map-loader"
+            }
+
         ]
     },
     plugins: [
-        new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js"),
+        new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.bundle.js' }),
         new webpack.DefinePlugin({
             'process.env': {
                 'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
