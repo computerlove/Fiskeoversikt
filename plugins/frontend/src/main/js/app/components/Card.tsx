d0faafd5f0b32ce8db3450bfd841d3526695f950
Marvin Bredal Lillehaug <marvin.lillehaug@kantega.no>
2017-04-17 18:40:44 +0200

    It shows details

--- plugins/frontend/src/main/js/app/components/Card.tsx
+++ plugins/frontend/src/main/js/app/components/Card.tsx
@@ -1,20 +1,61 @@
 import * as React from 'react';
-import {LandingsdataByLandingdate} from "../domain/domain";
+import {LandingsdataByLandingdate, Leveringslinje} from "../domain/domain";
 
 interface CardProps {
     landingsdata: LandingsdataByLandingdate
 }
-export default class Card extends React.Component<CardProps, {}> {
+interface CardState {
+    showDetails: boolean
+}
+export default class Card extends React.Component<CardProps, CardState> {
+    constructor(props: CardProps) {
+        super(props);
+        this.state = {
+            showDetails: false
+        }
+    }
+
     render(){
         const ld = this.props.landingsdata;
         const dato = ld.landingsdato.toString();
         return (
-            <article className="card" >
-                <h1>
-                    <span className="fartøy">{ld.fartøy}</span>
-                    <span className="landingsdato">{dato}</span>
-                </h1>
+            <article className="card" onClick={(e) => this.setState({showDetails:!this.state.showDetails})} >
+                <header>
+                    <h1>
+                        <span className="fartøy">{ld.fartøy}</span>
+                        <time className="landingsdato">{dato}</time>
+                    </h1>
+                </header>
+                <summary className="totalFangst">
+                    {totalFangst(ld.leveringslinjer)}kg
+                </summary>
+
+                {this.state.showDetails && <Details landingsdata={ld} /> }
             </article>
         );
+    }
+}
+
+function totalFangst(leveringslinjer: Array<Leveringslinje>): number {
+    return leveringslinjer.reduce((acc, curr) => acc + curr.nettovekt, 0);
+}
+
+class Details extends React.Component<CardProps, {}> {
+    render() {
+        const leveringslinjer = this.props.landingsdata.leveringslinjer;
+        return (
+            <div className="details">
+                <ul>
+                    {leveringslinjer.map(ll => (
+                        <li key={ll.id}>
+                            <span className="fiskeslag">{ll.fiskeslag}</span>
+                            <span className="tilstand">{ll.tilstand}</span>
+                            <span className="størrelse">{ll.storrelse}</span>
+                            <span className="kvalitet">{ll.kvalitet}</span>
+                            <span className="nettovekt">{ll.nettovekt}</span>
+                        </li>
+                    ))}
+                </ul>
+            </div>)
     }
 }
